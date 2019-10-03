e491378ecf94f25f53143f18778f7639fb5e7c9a
Dylan <dylan@dylanbyars.com>
2019-10-03 08:58:09 -0400

    make the projects more clickable

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 const PeriodicTableImg = () => (
-  <div className="periodic-table-grid-container h-32 w-64 mx-16">
+  <div className="periodic-table-grid-container h-32 w-64">
     <span className="alkali bg-grey text-grey">.</span>
     <span className="alkaline-earth bg-red-light text-red-light">.</span>
     <span className="metals bg-pink-light text-pink-light">.</span>
@@ -21,13 +21,12 @@ const PeriodicTableImg = () => (
 
 export default () => (
   <section className="projects flex flex-col justify-around items-center">
-    <article className="mx-auto my-8 flex flex-col items-center">
-      <PeriodicTableImg />
-      <p className="my-6">A wikipedia data powered periodic table.</p>
-      <Link className="link" to="/periodic-table">
-        Periodic Table
-      </Link>
-    </article>
+    <Link className="link" to="/periodic-table">
+      <article className="mx-auto my-8 flex flex-col items-center">
+        <PeriodicTableImg />
+        <p className="p-6">A wikipedia-powered periodic table built with React.</p>
+      </article>
+    </Link>
 
     {/* <article className="mx-auto my-8 flex flex-col items-center">
       <CellularAutomatonImg />
