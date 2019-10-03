1af5c7e71549d00181e1852a46324ea5c13458e9
Dylan <dylan@dylanbyars.com>
2019-10-02 22:04:44 -0400

    dancing man favicon 🕺

--- src/components/PageContainer.js
+++ src/components/PageContainer.js
@@ -6,7 +6,7 @@ export default ({ title, children }) => (
     <Helmet title={`Dylan Byars | ${title}`}>
       <link
         rel="icon"
-        href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/223/male-technologist_1f468-200d-1f4bb.png"
+        href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/223/man-dancing_1f57a.png"
       />
     </Helmet>
     {children}
