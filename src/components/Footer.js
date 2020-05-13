235f68857beee70d2208b40b3e05f096a28ab762
Dylan <dylan@dylanbyars.com>
2020-05-13 08:37:30 -0400

    2019 -> 2020

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default () => (
   <footer className="flex items-end justify-start">
     <small className="tracking-wide font-hairline">
-      &copy; 2019 Dylan Byars
+      &copy; 2020 Dylan Byars
     </small>
   </footer>
 )
